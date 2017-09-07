df531731d26c498a9e59911a6e2f2d4cf0f9b390
Kor Zhang <1138829222@qq.com>
2017-09-07 15:20:36 +0800

    配置springmvc的拦截器到项目并且测试
    配置springmvc的拦截器到项目并且测试

---
+++ src/com/zk/ssm/interceptor/HandlerInterceptor1.java
@@ -0,0 +1,48 @@
+package com.zk.ssm.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.zk.ssm.util.Util;
+
+/**
+ * Title:HandlerInterceptor1
+ * <p>
+ * Description:
+ * <p>
+ * @author Kor_Zhang
+ * @date 2017年9月7日 下午3:14:21
+ * @version 1.0
+ */
+public class HandlerInterceptor1 implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		
+		Util.l.info("HandlerInterceptor1#preHandle");
+		
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+
+		Util.l.info("HandlerInterceptor1#postHandle");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+
+		Util.l.info("HandlerInterceptor1#afterCompletion");
+
+	}
+
+}
